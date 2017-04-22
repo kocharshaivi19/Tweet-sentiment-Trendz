1f5d1d11feb3c6b5a867a8732c84cc97f0836a2d
kocharshaivi19 <sk6435@nyu.edu>
2017-04-22 13:00:10 -0400

    Adding images

--- Tweets/settings.py
+++ Tweets/settings.py
@@ -123,9 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/TweetMap/static/'
 STATIC_ROOT = 'static'
-MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR
 
