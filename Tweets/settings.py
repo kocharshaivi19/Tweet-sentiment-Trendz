5ab9acf1f8d226f19a255d374e92cf8cff283b0c
kocharshaivi19 <sk6435@nyu.edu>
2017-04-22 14:35:15 -0400

    Fixed marker issues

--- Tweets/settings.py
+++ Tweets/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'Tweets/static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_ROOT = BASE_DIR
 
